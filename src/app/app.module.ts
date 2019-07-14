5d1f18c09d7f6e62a8e01a130f3d500578969ebf
Adriano Silva <slivadrip@gmail.com>
2019-07-13 21:01:51 -0300

    commit initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { PainelNegociacaoComponent } from './painel-negociacao/painel-negociacao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PainelNegociacaoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
